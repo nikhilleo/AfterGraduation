d6b8d84a0f090688a3cd41b13d13ead16f1e8c74
nikhilleo <ramrakhyani94@gmail.com>
2021-02-03 01:39:59 +0530

    Basic Navbar And Home Component

---
+++ src/Components/HomePage/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import back from "./background.png"
+import "./Homepage.css"
+function Homepage() {
+    return (
+        <div className="main">
+            <div className="home__logo">
+
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+    
+    const [Toggle, setToggle] = useState(false)
+
+
+    return (
+        <div>
+           <nav>
+            <div className="logo">
+                After Graduation
+            </div>
+            <ul className="nav-links" style={{
+                transform : Toggle ? "translateX(0px)" : ""
+            }}>
+                <li><a>Home</a></li>
+                <li><a>Register</a></li>
+                <li><a>Job Postings</a></li>
+                <li><a>Contact Us</a></li>
+                <li><a>About Us</a></li>
+            </ul>
+            <i onClick={()=>setToggle(!Toggle)} className="fas fa-bars burger"></i>
+           </nav>
+        </div>
+    )
+}
+
+export default Navbar
