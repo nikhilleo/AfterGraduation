c176d523629f30eb809573c5f34d74a3cb8776e3
nikhilleo <ramrakhyani94@gmail.com>
2021-02-03 16:09:16 +0530

    Background Component

---
+++ src/Components/Background/Background.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Background.css"
+
+function Background() {
+    return (
+        <div className="back_custom">
+            <div className="back_design">
+                <div className="big_orange"></div>
+                <div className="small_orange"></div>
+                <div className="big_blue"></div>
+                <div className="mid_blue"></div>
+                <div className="small_blue"></div>
+                <div className="custom_logo"></div>
+            </div>
+            <div className="back_info">
+
+            </div>
+        </div>
+    )
+}
+
+export default Background
--- src/Components/HomePage/Homepage.js
+++ src/Components/HomePage/Homepage.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Background from "./../Background/Background"
 import back from "./background.png"
 import "./Homepage.css"
 function Homepage() {
     return (
         <div className="main">
+                <Background/>
             <div className="home__logo">
-
             </div>
         </div>
     )
