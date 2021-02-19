1cd4b9ba585a7d1915c54d99199e5649e24f708f
nikhilleo <ramrakhyani94@gmail.com>
2021-02-20 00:10:19 +0530

    Backend Connected

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    // baseURL:"https://aftergraduation.herokuapp.com/"
-    baseURL:"http://localhost:8080"
+    baseURL:"https://after-graduation-ck.herokuapp.com/"
+    // baseURL:"http://localhost:8080"
 })
 
 export default instance;
